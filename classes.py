d9eb604cc5eb48bcc6eafc39b5dd1e7d0b6e46a4
Oleksandr Veryk <veryk.av@gmail.com>
2025-03-11 23:41:48 +0200

    added attributes for class functionality

--- classes.py
+++ classes.py
@@ -1,18 +1,32 @@
 class Time:
     def __init__(self, hours, minutes, seconds):
-        self.hours = hours
-        self.minutes = minutes
+        self.hours = hours + minutes // 60
+        self.minutes = minutes % 60
         self.seconds = seconds
 
     def add_seconds(self, n):
+        total_seconds = (self.hours * 3600 + self.minutes * 60 + self.seconds) + n
+        self.hours = total_seconds // 3600
+        self.minutes = (total_seconds % 3600) // 60
+        self.seconds = total_seconds % 60
         print(f"add seconds: {n}")
 
     def subtract_seconds(self, n):
-        print("subtract seconds")
+        total_seconds = (self.hours * 3600 + self.minutes * 60 + self.seconds) - n
+        self.hours = total_seconds // 3600
+        self.minutes = (total_seconds % 3600) // 60
+        self.seconds = total_seconds % 60
+        print(f"subtract seconds {n}")
 
     def display(self):
-        print(f"{self.hours}:{self.minutes}:{self.seconds}")
+        print(f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}")
 
 
-my_watch = Time(2, 12, 34)
+my_watch = Time(0, 90, 0)
+my_watch.display()
+
+my_watch.add_seconds(95)
+my_watch.display()
+
+my_watch.subtract_seconds(360)
 my_watch.display()
