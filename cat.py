5707750643d1a813e58643cf869ce5681b3c90da
Oleksandr Veryk <veryk.av@gmail.com>
2025-04-02 16:34:28 +0300

    add new func

--- cat.py
+++ cat.py
@@ -1,6 +1,8 @@
 class Cat:
     def __init__(self, age):
         self.age = age
+        self. value_speed = self._set_average_speed()
+        self.saturation_level = 10
 
     def print_age(self):
         print(self.age)
@@ -8,17 +10,30 @@ class Cat:
     def _set_average_speed(self):
         if self.age <= 7:
             return 12
-        if self.age > 7 and self.age <= 10:
+        if 7 < self.age <= 10:
             return 9
         if self.age > 10:
             return 6
 
-    value_speed = _set_average_speed()
-#     Знайти як присвоїти значення приватного метода в змінну класу.
-# https://docs.python.org/uk/3.13/tutorial/classes.html#class-and-instance-variables
+    def _increase_saturation_level(self):
+        pass
 
-a = Cat(11)
+    def _reduce_saturation_level(self):
+        pass
+
+    def get_saturation_level(self):
+        result = f"Saturation level: {self.saturation_level}" if self.saturation_level > 0 else "Ваша кішка мертва :("
+        return result
+
+    def get_average_speed(self):
+        pass
+
+a = Cat(1)
 a.print_age()
+
 print(a.value_speed)
+print(a.saturation_level)
+print(a.get_saturation_level())
+
 
 
